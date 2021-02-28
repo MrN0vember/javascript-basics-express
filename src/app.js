1be0ccf21f037a4bfd8a924e5f498ea292ae90e8
James <j.a.pratt@outlook.com>
2021-02-28 17:32:16 +0000

    uppcase function added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
 const app = express();
-const { sayHello } = require('./lib/strings');
+const { sayHello, uppercase } = require('./lib/strings');
 
 app.get('/strings/hello/:string', (request, response) => {
   response.status(200).json({ result: sayHello(request.params.string) });
+});
+
+app.get('/strings/upper/:uppercase', (request, response) => {
+  response.status(200).json({ result: uppercase(request.params.uppercase) });
 });
 
 module.exports = app;
